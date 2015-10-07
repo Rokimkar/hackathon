cf795a3b2fdb5a51c7765272cf9cc69e9c61f24d
Sanchit Kumar Singh <Sanchit.Singh@timesinternet.in>
2015-10-07 19:09:30 +0530

    Initial Commit

---
+++ Hackathon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hackathon
+//
+//  Created by Sanchit Kumar Singh on 10/7/15.
+//  Copyright (c) 2015 Sanchit Kumar Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hackathon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hackathon
+//
+//  Created by Sanchit Kumar Singh on 10/7/15.
+//  Copyright (c) 2015 Sanchit Kumar Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
