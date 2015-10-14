c151f4824259fa01cbd8f8533755e03f3c3ab5dc
Sanchit Kumar Singh <Rokimkar@Sanchits-MacBook-Pro.local>
2015-10-15 00:24:50 +0530

    data display

--- Hackathon/WishViewController.m
+++ Hackathon/WishViewController.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    wishTableView = [[BaseTableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) andProductsArray:nil];
+    wishTableView = [[BaseTableView alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, self.view.frame.size.height) andProductsArray:nil];
     [self.view addSubview:wishTableView];
     // Do any additional setup after loading the view.
 }
