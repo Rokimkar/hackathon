12e9f3b980ec21375b723a288172f8c8bc561959
Kunal Chelani <53kunal@gmail.com>
2015-10-15 23:04:15 +0530

    json commit kunal

--- Hackathon/CategoryDetailViewController.m
+++ Hackathon/CategoryDetailViewController.m
@@ -9,6 +9,7 @@
 #import "CategoryDetailViewController.h"
 #import "SubcategorydetailViewController.h"
 #import "SubCategoryViewController.h"
+#import "ProductData.h"
 @interface  CategoryDetailViewController()
 {
     CGSize screenSize;
@@ -22,21 +23,31 @@
     self =  [super init];
     if (self) {
         screenSize=([UIScreen mainScreen]).bounds.size;
-        NSMutableArray* viewControllers = [NSMutableArray array];
-        for(int i = 0; i < 3; i++){
-            NSMutableDictionary *subDict = [subCategoryArray objectAtIndex:i];
-            SubcategorydetailViewController* vc = [[SubcategorydetailViewController alloc] initWithNibName:@"SubcategorydetailViewController" bundle:nil];
-            vc.view.frame = CGRectMake(0, 64, screenSize.width, screenSize.height-64);
-//            vc.subCategoryTitle = [subDict objectForKey:@"title"];
-            vc.subCategoryTitle = [NSString stringWithFormat:@"%d",i];
-            vc.dataArray = [subDict objectForKey:@"dataArray"];
-            
-            vc.view.backgroundColor = [UIColor colorWithWhite:0.3+0.05*i alpha:1.0];
-            [viewControllers addObject:vc];
-        }
         
-        [self setViewControllers:viewControllers];
-        
+            ProductData *prodData = [[ProductData alloc] init];
+            [prodData fetchDataFor:@"Clothing" withSuccess:^(NSMutableArray *data) {
+                
+                NSMutableArray* viewControllers = [NSMutableArray array];
+                for(int i = 0; i < 3; i++){
+                    NSMutableDictionary *subDict = [subCategoryArray objectAtIndex:i];
+                    SubcategorydetailViewController* vc = [[SubcategorydetailViewController alloc] initWithNibName:@"SubcategorydetailViewController" bundle:nil];
+                    vc.view.frame = CGRectMake(0, 64, screenSize.width, screenSize.height-64);
+                    //            vc.subCategoryTitle = [subDict objectForKey:@"title"];
+                    vc.subCategoryTitle = [NSString stringWithFormat:@"%d",i];
+//                    vc.dataArray = [subDict objectForKey:@"dataArray"];
+                    vc.dataArray = data;
+                    
+                    vc.view.backgroundColor = [UIColor colorWithWhite:0.3+0.05*i alpha:1.0];
+                    [viewControllers addObject:vc];
+                }
+                
+                [self setViewControllers:viewControllers];
+
+                
+                
+            } failure:^(NSError *error) {
+                
+            }];
     }
     
     return self;
